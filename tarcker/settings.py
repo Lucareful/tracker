7458c672ec9022e0c37556af1e81d30f00bd9a83
Luenci <787068756@qq.com>
2020-09-08 22:58:00 +0800

    update:短信接口开发

--- tarcker/settings.py
+++ tarcker/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'tarcker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
---
+++ utils/tencent/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Luenci
+@file: __init__.py.py
+@time: 9/8/2020 10:05 PM
+'''
