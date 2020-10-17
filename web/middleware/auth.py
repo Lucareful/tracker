ab668642bff957a937222e858df55eedba9124ef
Luenci <787068756@qq.com>
2020-10-17 13:20:04 +0800

    done:登录,注册,验证完成

---
+++ web/middleware/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Luenci
+@file: auth.py
+@time: 10/17/2020 12:53 PM
+"""
+from django.utils.deprecation import MiddlewareMixin
+
+from web import models
+
+
+class AuthMiddleware(MiddlewareMixin):
+    """
+    用户认证中间件
+    """
+
+    def process_request(self, request):
+        """
+        判断用户是否登录.
+        :param request:
+        :return:
+        """
+        user_id = request.session.get("user_id", 0)
+        print(request.session)
+        print(user_id)
+        user_obj = models.UserInfo.objects.filter(id=user_id).first()
+        request.tracker = user_obj
--- web/urls.py
+++ web/urls.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 # encoding: utf-8
-'''
+"""
 @author: Luenci
 @file: urls.py
 @time: 9/19/2020 2:24 PM
-'''
+"""
 
 from django.urls import path, include
 from django.conf.urls import url
 from web.views import account, home
 
 urlpatterns = [
-    url(r'^register/$', account.register, name='register'),
-    url(r'^login/sms/$', account.login_sms, name='login_sms'),
-    url(r'^login/$', account.login, name='login'),
-    url(r'^image/code/$', account.image_code, name='image_code'),
-    url(r'^send/sms/$', account.sendSms, name='send_sms'),
-    url(r'^index/$', home.index, name='index'),
-    url(r'^logout/$', account.logout, name='logout'),
+    url(r"^register/$", account.register, name="register"),
+    url(r"^login/sms/$", account.login_sms, name="login_sms"),
+    url(r"^login/$", account.login, name="login"),
+    url(r"^image/code/$", account.image_code, name="image_code"),
+    url(r"^send/sms/$", account.sendSms, name="send_sms"),
+    url(r"^index/$", home.index, name="index"),
+    url(r"^logout/$", account.logout, name="logout"),
+    # 项目列表
+    # url(r'^project/list/$', project.project_list, name='project_list'),
 ]
