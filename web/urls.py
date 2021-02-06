4bd81904547a277ad2551996f0097d0166a53b86
luenci <787068756@qq.com>
2021-02-06 12:32:21 +0800

    upadte:urls

--- web/urls.py
+++ web/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
                 ),
                 url(r"^wiki/upload/$", wiki.wiki_upload, name="wiki_upload"),
                 url(r"^file/$", file.file, name="file"),
-                # url(r'^file/delete/$', file.file_delete, name='file_delete'),
+                url(r"^file/delete/$", file.file_delete, name="file_delete"),
                 # url(r'^cos/credential/$', file.cos_credential, name='cos_credential'),
                 # url(r'^file/post/$', file.file_post, name='file_post'),
                 # url(r'^file/download/(?P<file_id>\d+)/$', file.file_download, name='file_download'),
