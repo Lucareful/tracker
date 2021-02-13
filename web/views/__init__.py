0aa55d86a905acd87e6d483ebf1212a824cfff94
luenci <787068756@qq.com>
2021-02-13 14:57:28 +0800

    add:pagination&forms

--- web/views/__init__.py
+++ web/views/__init__.py
@@ -5,3 +5,12 @@
 @file: __init__.py.py
 @time: 9/18/2020 10:26 PM
 """
+import account
+import file
+import issues
+import project
+import wiki
+import home
+from . import statistics
+import dashboard
+import setting
